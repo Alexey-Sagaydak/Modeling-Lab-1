e7685cfe8022cb1816bbd27d1a0823d6990eed74
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2023-02-19 14:52:03 +0700

    Добавьте файлы проекта.

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1082, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить гистограмму";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(533, 48);
+            this.chart1.Name = "chart1";
+            series13.ChartArea = "ChartArea1";
+            series13.Legend = "Legend1";
+            series13.Name = "NormDist";
+            series14.BorderWidth = 3;
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series14.Color = System.Drawing.Color.Red;
+            series14.Legend = "Legend1";
+            series14.Name = "normCurve";
+            this.chart1.Series.Add(series13);
+            this.chart1.Series.Add(series14);
+            this.chart1.Size = new System.Drawing.Size(500, 596);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1069, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(99, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Генерация чисел методом усечения";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1068, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1107, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите k";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1068, 212);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(143, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1068, 254);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(143, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1064, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Выборочное среднее";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1064, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Среднее квадр. отклон.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(1079, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Хи квадрат набл.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(1096, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Хи квадрат кр.";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1068, 296);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(144, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoEllipsis = true;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1073, 396);
+            this.label7.MaximumSize = new System.Drawing.Size(150, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 30;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // chart2
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chart2.Legends.Add(legend8);
+            this.chart2.Location = new System.Drawing.Point(12, 48);
+            this.chart2.Name = "chart2";
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series15.Color = System.Drawing.Color.Red;
+            series15.Legend = "Legend1";
+            series15.Name = "theorY";
+            series16.ChartArea = "ChartArea1";
+            series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series16.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            series16.Legend = "Legend1";
+            series16.Name = "seenY";
+            this.chart2.Series.Add(series15);
+            this.chart2.Series.Add(series16);
+            this.chart2.Size = new System.Drawing.Size(515, 596);
+            this.chart2.TabIndex = 14;
+            this.chart2.Text = "chart2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(587, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(345, 18);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Гистограмма частот генеральной совокупности";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 656);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        int N = 1000; // кол-во сгенерированных чисел
+        double M = 0.5; // данные для интервала
+        int INTERVAL_AMOUNT = 20;
+        double SIGMA = 0.16;
+        // k = 3 покрывает весь интервал 0 - 1
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            
+            // Массив значений выборки
+            double[] x = new double[N]; 
+            Random r = new Random();
+
+            // Получение k из textBox1
+            double k = Convert.ToDouble(textBox1.Text);
+
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+
+            for (int i = 0; i < N; i++)
+            {
+                while (true)
+                {
+                    double randX = r.NextDouble();
+
+                    if (randX < M - SIGMA * k || randX > M + SIGMA * k) continue;
+
+                    double randY = r.NextDouble() * (2.5 - 0) + 0;
+                    double theorY = Math.Exp(-(((randX - M) * (randX - M)) / (2 * SIGMA * SIGMA)))
+                        / (SIGMA * Math.Sqrt(2 * Math.PI));
+
+                    if (randY <= theorY)
+                    {
+                        x[i] = randX;
+                        chart2.Series[0].Points.AddXY(randX, theorY);
+                        chart2.Series[1].Points.AddXY(randX, randY);
+                        break;
+                    }
+                }
+            }
+            
+            double max = x.Max(), min = x.Min();
+            
+            // Длина интервала
+            double intervalLen = (max - min) / INTERVAL_AMOUNT;
+            
+            // Середины интервалов
+            double[] middles = new double[INTERVAL_AMOUNT];
+
+            // Частоты попаданий значений в интервалы
+            double[] frequencies = new double[INTERVAL_AMOUNT];
+
+            // Значения нормальной кривой
+            double[] n = new double[INTERVAL_AMOUNT];
+
+            // Заполнение массивов середин интервалов и частот
+            for (int i = 0; i < INTERVAL_AMOUNT; i++) 
+            {
+                int amountInInterval = 0;
+                double leftBorder = min + intervalLen * i;
+                double rightBorder = min + intervalLen * (i + 1);
+          
+                for (int j = 0; j < x.Length; j++)
+                    if (x[j] >= leftBorder && x[j] < rightBorder) amountInInterval++;
+
+                frequencies[i] = amountInInterval;
+                middles[i] = (leftBorder + rightBorder) / 2;
+            }
+
+            // Выборочное среднее
+            double sampleMean = getSampleMean(middles, frequencies);
+
+            // Среднее квадратическое отклонение
+            double standardDeviation = getDeviation(middles, frequencies, sampleMean);
+
+            // Хи квадрат наблюдаемое, кол-во степеней свободы
+            double chiSquareSeen = 0, degree = INTERVAL_AMOUNT - 2 - 1;
+            
+            // Сумма частот
+            double freqSum = excel.WorksheetFunction.Sum(frequencies);
+
+            // Хи квадрат критическое
+            double chiSquareCrit = excel.WorksheetFunction.ChiInv(0.05, degree);
+
+            // Считаем точки для нормальной кривой, а также хи квадрат наблюдаемое
+            for (int i = 0; i < INTERVAL_AMOUNT; i++)
+            {
+                // Значение функции плотности нормального распределения
+                double t = excel.WorksheetFunction.NormDist((middles[i] - sampleMean) / standardDeviation, 0, 1, false);
+
+                // Теоретические частоты
+                n[i] = intervalLen * freqSum / standardDeviation * t;
+                chiSquareSeen += (((frequencies[i] - n[i]) * (frequencies[i] - n[i])) / n[i]);
+            }
+            
+            // вывод данных на экран в поля textBox(2-5)
+            textBox2.Text = sampleMean.ToString();
+            textBox3.Text = standardDeviation.ToString();
+            textBox4.Text = chiSquareSeen.ToString();
+            textBox5.Text = chiSquareCrit.ToString();
+            
+            // Проверка критерия Пирсона
+            if (chiSquareSeen > chiSquareCrit) 
+                label7.Text = "Согласно критерию Пирсона генеральная совокупность не распределена нормально";
+            else 
+                label7.Text = "Согласно критерию Пирсона генеральная совокупность распределена нормально";
+
+            excel.Quit();
+            buildHist(frequencies, n);
+            
+        }
+        double getDeviation(double[] mid, double[] freq, double sampleMean) // ср. квадр. откл
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            double deviation = 0;
+            for (int i = 0; i < mid.Length; i++)
+                deviation += mid[i] * mid[i] * freq[i];
+            deviation = Math.Sqrt((deviation / excel.WorksheetFunction.Sum(freq)) - sampleMean * sampleMean);
+
+            excel.Quit();
+            return deviation;
+        }
+        double getSampleMean(double[] mid, double[] freq) // выбор. среднее
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            double sampleMean = 0;
+            for (int i = 0; i < mid.Length; i++)
+                sampleMean += mid[i]*freq[i];
+            sampleMean /= excel.WorksheetFunction.Sum(freq);
+
+            return sampleMean;
+        }
+
+        // Построение гистограммы и нормальной кривой
+        void buildHist(double[] histFreq, double[] normCurve)
+        {
+            // Очищаем гистограмму
+            chart1.Series[0].Points.Clear(); 
+            chart1.Series[1].Points.Clear();
+            // Построение гистограммы
+            for (int i = 0; i < INTERVAL_AMOUNT; i++)
+            {
+               chart1.Series[0].Points.AddXY(i, histFreq[i]);
+               chart1.Series[1].Points.AddXY(i, normCurve[i]);
+            }
+        }
+    }
+}
